d73ba62ca5daafc6325097e5b6280c8c893f4bd4
Berny Cantos <berny.cantos@ofertix.com>
2014-03-06 16:41:28 +0100

    add UserParamConverter
    UserParamConverter checks your Controller for parameters named `$user` and injects the current user from `SecurityContext` (if it exists)

---
+++ src/Ofertix/SecurityExtraBundle/ParamConverter/UserParamConverter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ofertix\SecurityExtraBundle\ParamConverter;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ConfigurationInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class UserParamConverter implements ParamConverterInterface
+{
+    /** @var SecurityContext */
+    protected $securityContext;
+
+    public function __construct(SecurityContext $securityContext)
+    {
+        $this->securityContext = $securityContext;
+    }
+
+    public function apply(Request $request, ConfigurationInterface $configuration)
+    {
+        /** @var $configuration ParamConverter */
+        $user = $this->securityContext->getToken()->getUser();
+        if ($user) {
+            $request->attributes->set($configuration->getName(), $user);
+        }
+    }
+
+    public function supports(ConfigurationInterface $configuration)
+    {
+        return $configuration instanceof ParamConverter && $configuration->getName() === 'user';
+    }
+}
